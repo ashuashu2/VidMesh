3517420fc24dadb942c28f6b098a540520ea1e1d
ashutosh birthare <ashubirthare.123@gmail.com>
2022-11-15 09:39:10 +0530

    update

--- src/pages/Authentication/Signup.jsx
+++ src/pages/Authentication/Signup.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 // import "../Authentication/LoginSignup.css"
 // import "../Authentication/LoginSignup.css"
+import "../Authentication/LoginSignup.css"
+
 
 function Signup() {
   return (
